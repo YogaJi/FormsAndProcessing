7bd5ea4c028a6572ab697e536e98a3c0788d28f3
YogaJi <y.disturbia@gmail.com>
2021-12-14 00:39:04 +0800

    Initial commit

---
+++ FormsAndProcessing/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndProcessing.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        
+        [Required]
+        [Range(0,int.MaxValue)]
+        [FromForm]
+        public int NumOfDate { get; set; }
+
+        [Required]
+        [FromForm]
+        public DateTime Dates { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? NewDate { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (ModelState.IsValid) {
+            }
+
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            { 
+                NewDate = Dates.AddDays(NumOfDate);
+            }
+        }
+    }
+
+ 
+}
